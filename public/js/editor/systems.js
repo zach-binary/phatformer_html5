d6120e513b9ad8ae87daf83a44fe33ebb9e67798
Zach Burke <zachburke@gamestop.com>
2014-04-18 22:08:43 -0500

    showing grid when tilemaps are clicked

--- public/js/editor/systems.js
+++ public/js/editor/systems.js
@@ -1,5 +1,34 @@
 (function() {
 
-	
+	window.Systems = {};
+
+	window.Systems.tilemap = function(e, c) {
+		Graphics.context.save();
+		Graphics.context.translate(-Client.offset.x, -Client.offset.y);
+		
+		_drawGrid(e, c);
+
+		Graphics.context.restore();
+	};
+
+	var _drawGrid = function(e, c) {
+		var width = c.width * 16,
+			height = c.height * 16,
+			i;
+
+		Graphics.context.strokeStyle = 'rbga(200, 200, 90, 0.7)'
+		Graphics.context.beginPath();
+
+		for (i = 0; i <= width; i += 16) {
+			Graphics.context.moveTo(e.body.x + i, e.body.y);
+			Graphics.context.lineTo(e.body.x + i, e.body.y + height);
+		}
+
+		for (i = 0; i <= height; i+= 16) {
+			Graphics.context.moveTo(e.body.x, e.body.y + i);
+			Graphics.context.lineTo(e.body.x + width, e.body.y + i);
+		}
+		Graphics.context.stroke();
+	}	
 
 })();
